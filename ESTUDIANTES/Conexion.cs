77af4ce3e2c3b2b485d5029387a7e18fdb5b5334
danie <danie@DESKTOP-J2QNRUR>
2023-06-14 20:21:40 -0500

    Agregar archivos de proyecto.

---
+++ ESTUDIANTES/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ESTUDIANTES
+{
+    class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER=(local); DATABASE=crud; integrated security = true");
+            cn.Open();
+            return cn;
+        }
+    }
+}
---
+++ ESTUDIANTES/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ESTUDIANTES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            MessageBox.Show("Conexión Exitosa");
+
+            dataGridView1.DataSource = llenar_grid();
+        }
+
+        public DataTable llenar_grid()
+        {
+            Conexion.Conectar();
+            DataTable dt = new DataTable();
+            string consulta = "SELECT * FROM ALUMNO";
+            SqlCommand cmd = new SqlCommand(consulta, Conexion.Conectar());
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            string insertar = "INSERT INTO ALUMNO (CODIGO, NOMBRES, APELLIDOS, " +
+                              "DIRECCION)VALUES(@CODIGO, @NOMBRES, @APELLIDOS," +
+                              "@DIRECCION)";
+            SqlCommand cmd1 = new SqlCommand(insertar, Conexion.Conectar());
+            cmd1.Parameters.AddWithValue("@CODIGO", txtcodigo.Text);
+            cmd1.Parameters.AddWithValue("@NOMBRES", txtnombres.Text);
+            cmd1.Parameters.AddWithValue("@APELLIDOS", txtapellidos.Text);
+            cmd1.Parameters.AddWithValue("@DIRECCION", txtdireccion.Text);
+
+            cmd1.ExecuteNonQuery();
+
+            MessageBox.Show("Registro completado con exito");
+
+            dataGridView1.DataSource = llenar_grid();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtcodigo.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtnombres.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtapellidos.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                txtdireccion.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            string actualizar = "UPDATE ALUMNO SET CODIGO= @CODIGO, NOMBRES = @NOMBRES," +
+                                "APELLIDOS = @APELLIDOS, DIRECCION = @DIRECCION WHERE " +
+                                "CODIGO = @CODIGO";
+            SqlCommand cmd2 = new SqlCommand(actualizar, Conexion.Conectar());
+
+            cmd2.Parameters.AddWithValue("@CODIGO", txtcodigo.Text);
+            cmd2.Parameters.AddWithValue("@NOMBRES", txtnombres.Text);
+            cmd2.Parameters.AddWithValue("@APELLIDOS", txtapellidos.Text);
+            cmd2.Parameters.AddWithValue("@DIRECCION", txtdireccion.Text);
+
+            cmd2.ExecuteNonQuery();
+
+            MessageBox.Show("Sus datos se actualizaron correctamente");
+            dataGridView1.DataSource = llenar_grid();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Conexion.Conectar();
+            string eliminar = "DELETE FROM ALUMNO WHERE CODIGO = @CODIGO";
+            SqlCommand cmd3 = new SqlCommand(eliminar, Conexion.Conectar());
+            cmd3.Parameters.AddWithValue("@CODIGO", txtcodigo.Text);
+
+            cmd3.ExecuteNonQuery();
+
+            MessageBox.Show("El usuario se elimino con exito");
+
+            dataGridView1.DataSource = llenar_grid();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtcodigo.Clear();
+            txtnombres.Clear();
+            txtapellidos.Clear();
+            txtdireccion.Clear();
+
+            txtcodigo.Focus();
+        }
+    }
+}
